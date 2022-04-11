1d02a16ee5fd2efb2fcb028a70a9df5c239496f3
Pakete <francisco.saenzr@redsys.es>
2022-04-11 13:33:22 +0200

    Terminal 20 por defecto

--- Config.php
+++ Config.php
@@ -10,7 +10,7 @@ if (!class_exists('Config')) {
     {
         // Merchant identification
         public static $FUC = "999008881";
-        public static $TERMINAL = "1";
+        public static $TERMINAL = "20";
 
         // This has to be obtained from your system and not directly written into code, this is just an example coded to be functional out of the box
         public static $PRIVATEKEY_SANDBOX = "sq7HjrUOBfKmC576ILgskD5srU870gJ7";
